877aace4d5c93787801eb1d23d2c19a6dfc06594
Esther AISSAH <135969404+aissahesther@users.noreply.github.com>
2024-03-21 09:03:21 +0100

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,45 @@
+#sorting a list Permanenty With sort() method
+
+cars = ['bmw','audi','toyota','subaru']
+
+print (cars)
+
+cars.sort()
+
+print(cars)
+
+#___ reversing alphabetical order
+
+cars.sort(reverse = True)
+
+print(cars)
+
+# Sorting a List Temporarily with the sorted() Function
+
+cars = ['bmw','audi','toyota','subaru']
+
+
+print("Here is the original list :")
+
+print(cars)
+
+print("\nHere is the sorted List : ")
+
+sorted_list = sorted(cars)
+print(sorted_list)
+
+
+print("\nHere is the original list again :")
+print(cars)
+
+#Printing a List in Reverse Order
+
+cars.reverse()
+
+print(cars)
+
+cars = ['bmw','audi','toyota','subaru']
+#Finding the Length of a List
+
+print (len(cars))
+
